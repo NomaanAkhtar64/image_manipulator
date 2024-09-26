69b595a3aaf27ed8ce7e00bd854018f4d8f78ee9
NomaanAkhtar64 <nomaan.akhtar2@gmail.com>
2024-09-26 10:15:27 +0500

    ignore-fix

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { ImageRouter } from './routes/image';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import { ImageRouter } from "./routes/image";
+import cors from "cors";
 
-dotenv.config({ path: './dev.env' });
+dotenv.config({ path: "./dev.env" });
 
 const app = express();
 
 app.use(cors());
-app.use(express.static('./media'));
+// app.use(express.static("./media"));
+app.use(express.static("./public"));
 
-app.use('/api/image/', ImageRouter);
+app.use("/api/image/", express.static("./media"), ImageRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Started at ${process.env.PORT}`);
